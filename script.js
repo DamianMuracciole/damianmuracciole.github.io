329aed7ca89eb4a2e88a0b575a99c108c1099591
DamianMuracciole <damianmuracciole@gmail.com>
2023-07-26 07:57:23 -0300

    agregado seccion 'ABOUT ME' con una base de que redactar

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () =>{
   const cafeArte = document.getElementById('cafeArte');
   const humulus = document.getElementById('humulus')
   const worldCup = document.getElementById('worldCup');
-
+  const proyectsAnimations = document.getElementsByClassName('proyects-animations');
   
 
   //------ Lógica para pasar de fondo transparente a fondo con color del header en el index.
@@ -26,6 +26,7 @@ window.addEventListener('load', () =>{
   });
 
   let scrollEfects = () => {
+    // Efectos para el home
     if(window.scrollY > window.innerHeight/4) {
       header[0].classList.add('solid-navBar')
     } else {
@@ -38,6 +39,24 @@ window.addEventListener('load', () =>{
       name.classList.remove('tittle-1-off');
       description.classList.add('tittle-1-off');
     }
+
+    // efectos para proyectos
+    if(window.scrollY > proyectsAnimations[0].getBoundingClientRect().top - 100){
+      proyectsAnimations[0].classList.add('proyects-animations-active');
+    } else if (window.scrollY < proyectsAnimations[0].getBoundingClientRect().top + 50){
+      proyectsAnimations[0].classList.remove('proyects-animations-active');
+    }
+    if(window.scrollY > proyectsAnimations[1].getBoundingClientRect().top - 50){
+      proyectsAnimations[1].classList.add('proyects-animations-active');
+    } else if (window.scrollY < proyectsAnimations[1].getBoundingClientRect().top + 50){
+      proyectsAnimations[1].classList.remove('proyects-animations-active');
+    }
+    if(window.scrollY > proyectsAnimations[2].getBoundingClientRect().top - 50){
+      proyectsAnimations[2].classList.add('proyects-animations-active');
+    } else if (window.scrollY < proyectsAnimations[2].getBoundingClientRect().top + 50){
+      proyectsAnimations[2].classList.remove('proyects-animations-active');
+    }
+
   }
 
   let popupWindow = (uri, title,dim) => { 
